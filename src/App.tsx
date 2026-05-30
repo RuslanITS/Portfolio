6f2620ec376c9c842cd956640b2a516d01e09d15
Ruslan <rcernov442@gmail.com>
2026-05-31 01:27:49 +0600

    add navigation routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Heater.tsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./containers/Home/Home.tsx";
+import Projects from "./containers/Projects/Projects.tsx";
+import Skills from "./containers/Skills/Skills.tsx";
+import Contacts from "./containers/Contacts/Contacts.tsx";
 
 const App = () => (
+  <>
     <Header />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/contacts" element={<Contacts />} />
+    </Routes>
+  </>
 );
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import { ToastContainer } from "react-toastify";
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
